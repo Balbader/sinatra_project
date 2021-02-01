05f4382c302d69d059f5c53e48f90f6a0c8ff445
Theo CAZENAVE <theocazenavecoupet@gmail.com>
2021-02-01 11:36:00 +0100

    save gossip and add gossip in /

--- lib/Gossip.rb
+++ lib/Gossip.rb
@@ -2,6 +2,8 @@ require 'csv'
 
 class Gossip
 
+    attr_accessor :author, :content
+
     def initialize(author, content)
       @author = author
       @content = content
@@ -13,6 +15,13 @@ class Gossip
         end
     end
 
+    def self.all 
+        all_gossips = []
+        CSV.read("./db/gossip.csv").each do |csv_line|
+            all_gossips << Gossip.new(csv_line[0], csv_line[1])
+          end
+        return all_gossips
+    end
 end
 
 
--- lib/controller.rb
+++ lib/controller.rb
@@ -3,22 +3,16 @@ require 'Gossip'
 class ApplicationController < Sinatra::Base
   
   get '/' do
-    erb :index 
+    erb :index, locals: {gossips: Gossip.all} 
   end
   
   get '/gossip/new/' do
     erb :new_gossip
   end
   
-  post '/gossip/new/potin/' do
+  post '/gossip/new/' do
     Gossip.new(params[:author],params[:content]).save
+    redirect '/'
   end
 
-  post '/gossip/new/potin/' do
-    puts "Salut, je suis dans le serveur"
-    puts "Ceci est le contenu du hash params : #{params}"
-    puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["author"]}"
-    puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["content"]}"
-    puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
-  end
 end
