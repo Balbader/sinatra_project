ef16b1a9a238742b6300d363792b0f54ce532deb
Balbader <15971200+Balbader@users.noreply.github.com>
2021-02-01 11:35:55 +0100

    initial

--- lib/gossip.rb
+++ lib/gossip.rb
@@ -2,17 +2,27 @@ require 'csv'
 
 
 class Gossip
-   attr_accessor :author, :content
+  attr_accessor :author, :content
 
   def initialize(author, content)
-      @content = content
-      @author = author
+    @content = content
+    @author = author
   end
-
+   
   def save
-    CSV.open("../db/gossip.csv", "ab") do |csv|
+    CSV.open("./db/gossip.csv", "ab") do |csv|
       csv << [@author, @content]
     end
+  end
+
+  def self.all
+      all_gossips = []
+
+      CSV.read("../db/gossip.csv").each do |i|
+        gossip_temp = Gossip.new(i[0],i[1])
+        all_gossips << gossip_temp
+      end
+      return all_gossips
   end
 
 end
